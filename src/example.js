238062fe27ec52a4f849b03a09e18d84003186dc
Nopparit <nopparip@scg.com>
2019-04-02 10:33:20 +0700

    aaa

--- src/example.js
+++ src/example.js
@@ -10,19 +10,26 @@ let arr = [
   ['X', 'O', 'O']
 ];
 
-console.log(findWin(arr));
 
-function findWin(arr) {
+ function findWin(arr) {
+
+  let isIngame = false;
 
   for (i = 0; i < 3; i++) {
     if ((arr[i][0] === arr[i][1] && arr[i][0] === arr[i][2]) || (arr[0][i] === arr[1][i] && arr[0][i] === arr[2][i])) {
       return arr[i][0] + ' Win';
     }
+
+    for(j = 0 ; j < 3 ; j++) {
+      if(arr[i][j] === ' ')
+      isIngame = true;
+    }
   }
 
+  
   if ((arr[0][0] === arr[1][1] && arr[0][0] === arr[2][2]) || (arr[0][2] === arr[1][1] && arr[0][2] === arr[2][0])) {
     return arr[1][1] + ' Win';
   }
 
-  return 'In game';
+  return isIngame ? 'In Game' : 'Draw';
 }
