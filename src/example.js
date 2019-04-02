227ad0333f95092861bfd73cb2db281a4eed2dd5
ชยากร เจนพิทักษ์ <bankj123@gmail.com>
2019-04-02 10:09:17 +0700

    Update first

--- src/example.js
+++ src/example.js
@@ -1,3 +1,36 @@
-export function  alwaysReturn1() {
-  return 1;
+import _ from "lodash";
+
+let dataVertical = [[],[],[]];
+let print = 'in game';
+
+export function main(input) {
+  checkData(input,'Horizontal');
+  if(print === 'in game'){
+    checkData(dataVertical,'Vertical');
+    if((input[0][0] === 'o' && input[1][1] === 'o' && input[2][2] === 'o') || (input[0][2] === 'o' && input[1][1] === 'o' && input[2][0] === 'o')){
+      print = 'O WIN'
+    }
+    if((input[0][0] === 'x' && input[1][1] === 'x' && input[2][2] === 'x') || (input[0][2] === 'x' && input[1][1] === 'x' && input[2][0] === 'x')){
+      print = 'X WIN'
+    }
+  }
+  return print;
 }
+
+function checkData(input,task){
+  input.some((res,key)=>{
+    if(task === 'Horizontal'){
+      res.forEach((value,keyin)=>{
+        dataVertical[keyin].push(value);
+      })
+    }
+    return _.countBy(res, x => x === 'o').true === 3 ? print = 'O WIN' : 
+    _.countBy(res, x => x === 'x').true === 3 ? print = 'X WIN' : false;
+  });
+}
+
+console.log(main([
+  ["x", "o", "o"],
+  ["x", "o", "x"],
+  ["o", "o", "x"]
+]));
