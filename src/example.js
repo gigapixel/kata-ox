5436fa3953dccf3cf8048b50e0434e59d183f3e5
Nopparit <nopparip@scg.com>
2019-04-02 10:09:06 +0700

    aaa

--- src/example.js
+++ src/example.js
@@ -1,3 +1,28 @@
-export function  alwaysReturn1() {
-  return 1;
+// let arr = [
+//   ['X', 'X', 'O'],
+//   ['X', 'O', 'X'],
+//   ['O', 'X', 'O']
+// ];
+
+let arr = [
+  ['X', 'X', 'O'],
+  ['O', 'X', 'X'],
+  ['X', 'O', 'O']
+];
+
+console.log(findWin(arr));
+
+function findWin(arr) {
+
+  for (i = 0; i < 3; i++) {
+    if ((arr[i][0] === arr[i][1] && arr[i][0] === arr[i][2]) || (arr[0][i] === arr[1][i] && arr[0][i] === arr[2][i])) {
+      return arr[i][0] + ' Win';
+    }
+  }
+
+  if ((arr[0][0] === arr[1][1] && arr[0][0] === arr[2][2]) || (arr[0][2] === arr[1][1] && arr[0][2] === arr[2][0])) {
+    return arr[1][1] + ' Win';
+  }
+
+  return 'In game';
 }
