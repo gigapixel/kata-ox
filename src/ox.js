8c6e91c21c84f9d3da6bb692b2d2f007c879287c
Teerayut2545 <teerayut2545@gmail.com>
2019-04-02 10:25:55 +0700

    commit, exclude IF

--- src/ox.js
+++ src/ox.js
@@ -1,55 +1,49 @@
 export class OX {
  
   checkGame(input) {        
+    let winResult = null;
     for (let pos=0; pos<3; pos++) {
       const rowData = input[pos];      
 
-      // Check Row
-      if (rowData[0] === rowData[1] && rowData[0] === rowData[2])      
-        return rowData[0];
-
-      // Check Column
-      if (input[0][pos] === input[1][pos] && input[0][pos] === input[2][pos])      
-        return input[0][pos];    
+      winResult = (!winResult && (
+                       (rowData[0] === rowData[1] && rowData[0] === rowData[2])
+                    || (input[0][pos] === input[1][pos] && input[0][pos] === input[2][pos]))
+                  ) 
+                  ? rowData[0] 
+                  : winResult;    
     }
 
     // Check Diag
-    let cell = input[0][0];
-    if (cell === input[1][1]  && cell === input[2][2]) return cell;
+    let cell1 = input[0][0];
+    winResult = (!winResult && (cell1 === input[1][1]  && cell1 === input[2][2])) ? cell1 : winResult;
 
-    cell = input[2][0];
-    if (cell === input[1][1] && cell === input[0][2]) return cell;
+    let cell2 = input[2][0];
+    winResult = (!winResult && (cell2 === input[1][1] && cell2 === input[0][2])) ? cell2 : winResult;
 
-    return null;
+    return winResult;
   }
 
   playOX(input) {
     let result = this.checkGame(input);
+            
+    const filter = input.filter((val) => {
+        return (val[0] === ' '  || val[1] === ' ' || val[2] === ' ')            
+    });      
+
+    const txtResult = (filter.length > 0) ? "In Game" : "Draw";      
     
 
-    if (!result) {
-      const filter = input.filter((val) => {
-          return (val[0] === ' '  || val[1] === ' ' || val[2] === ' ')            
-      });      
-
-      return (filter.length > 0) ? "In Game" : "Draw";      
-    }
-
-    return 'Winner: ' + result;
+    return (result) ? 'Winner: ' + result : txtResult;    
   }
 }
 
 const ox = new OX();
-// let input = [
-//   ['O', 'O', 'X'],
-//   ['X', 'X', 'O'],
-//   ['O', 'O', 'X'],
-// ]
+
 
 let input = [
   ['O', 'X', 'X'],
-  ['X', 'O', ' '],
-  ['O', 'X', 'O'],
+  ['X', 'X', 'X'],
+  ['X', 'O', 'O'],
 ]
 
 let g = ox.playOX(input);
